14b00b36fdb66e8fbc66b0483234c833148b8ce1
Fishcake <napat320282@gmail.com>
2022-02-10 14:52:29 +0700

    Connected with MongoDB Atlas

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-//Route files
-const hospitals = require('./routes/hospitals')
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
+//Connect to database
+connectDB();
+
+//Route files
+const hospitals = require('./routes/hospitals')
+
 const app = express();
 
+//Body parser
+app.use(express.json());
 
-/*
-app.get('/', (req,res) => {
-    res.send('<h1>Hello from express</h1>');
-    //res.send({name:'Brad'});
-    //res.json({name:'Brad'});
-    //res.sendStatus(400);
-    //res.status(400).json({success:false});
-    //res.status(200).json({success:true, data:{id:1}});
-});
-*/
+app.use('/api/v1/hospitals',hospitals)
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
 
-//Mount routers
-app.use('/api/v1/hospitals',hospitals)
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server % exit process
+    server.close(() => process.exit(1));
+})
