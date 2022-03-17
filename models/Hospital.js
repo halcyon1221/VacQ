2cd4d10087b7fbc8c672b611a999678bcfa141a6
Fishcake <napat320282@gmail.com>
2022-03-17 15:08:26 +0700

    Appointment

--- models/Hospital.js
+++ models/Hospital.js
@@ -32,6 +32,24 @@ const HospitalSchema = new mongoose.Schema({
         type: String,
         required: [true,'Please add a region']
     }
+},{
+    toJSON: {virtuals:true},
+    toObject: {virtuals:true}
+});
+
+//Reversepopulate with virtuals
+HospitalSchema.virtual('appointments',{
+    ref: 'Appointment',
+    localField: '_id',
+    foreignField: 'hospital',
+    justOne: false
+});
+
+//Cascade delete appointment when delete hospital
+HospitalSchema.pre('remove',async function(next){
+    console.log(`Appointmentts being removed from hospital ${this._id}`);
+    await this.model('Appointment').deleteMany({hospital:this._id});
+    next();
 });
 
 module.exports = mongoose.model('Hospital', HospitalSchema);
--- server.js
+++ server.js
@@ -10,8 +10,9 @@ dotenv.config({path:'./config/config.env'});
 connectDB();
 
 //Route files
-const hospitals = require('./routes/hospitals')
+const hospitals = require('./routes/hospitals');
 const auth = require('./routes/auth');
+const appointments=require('./routes/appointments');
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 
 app.use('/api/v1/hospitals',hospitals)
 app.use('/api/v1/auth',auth);
+app.use('/api/v1/appointments',appointments)
 
 const PORT = process.env.PORT || 5000;
 
