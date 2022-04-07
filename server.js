4879b5cc3cf56ee5ff914c537b8ad21ada0b51ff
Fishcake <napat320282@gmail.com>
2022-04-07 14:01:21 +0700

    add swagger

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const rateLimit=require('express-rate-limit');
 const hpp=require('hpp');
 const cors=require('cors');
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
@@ -21,6 +24,27 @@ const auth = require('./routes/auth');
 const appointments=require('./routes/appointments');
 
 const app = express();
+
+const swaggerOptions={
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ App'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000/api/v1'
+            }
+        ],
+
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //Body parser
 app.use(express.json());
